156faa0d864518dffa80bd1b5c73723371cd2945
cmirand4 <115188677+cmirand4@users.noreply.github.com>
2026-06-16 14:08:15 -0700

    Delete figureControl.m

--- figureControl.m
+++
@@ -1,5 +0,0 @@
-function [figs, figName] = figureControl(figs)
-% Wrapper function for backward compatibility
-% This function calls figOpen with the same parameters
-[figs, figName] = figOpen(figs);
-end
